b4ce4d9ce80acb70ed34ef6eed702cb7e9394ffd
0xAA55 <weixiaobao213@qq.com>
2021-10-24 05:16:51 +0800

    The file for the glorious int main()

---
+++ entry.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+#include"fontvideo.h"
+
+int main(int argc, char **argv)
+{
+    char *input_file = NULL;
+    fontvideo_p fv = NULL;
+    FILE *fp_out = NULL;
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s <input> [output]\n", argv[0]);
+        return 1;
+    }
+
+    input_file = argv[1];
+
+    if (argv >= 3)
+    {
+        fp_out = fopen(argv[2], "w");
+        if (!fp_out)
+        {
+            perror("Trying to open output file");
+        }
+    }
+    else
+    {
+        fp_out = stdout;
+    }
+
+    fv = fv_create(input_file, stderr, 80, 25);
+    if (!fv) goto FailExit;
+
+    while (fv_poll_show(fv, fp_out));
+
+    fv_destroy(fv);
+    if (fp_out != stdout) fclose(fp_out);
+    return 0;
+FailExit:
+    fv_destroy(fv);
+    if (fp_out != stdout && fp_out != NULL) fclose(fp_out);
+    return 2;
+}
